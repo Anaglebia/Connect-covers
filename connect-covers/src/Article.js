c4c6f22d9b45a31da91e32a3e73a3692c03713dc
Anaglebia <bia22.sp@icloud.com>
2023-04-18 11:51:12 -0300

    Criei os componentes inicias

---
+++ connect-covers/src/Article.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Article extends React.Component{
+    render(){
+      return (
+        <div>
+         <h1>Main</h1>
+        </div>
+      );
+    }
+  }
+
+  export default Article;
---
+++ connect-covers/src/Headers.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Headers extends React.Component{
+    render(){
+      return (
+        <div>
+         <h1>Cabeçalho</h1>
+        </div>
+      );
+    }
+  }
+
+  export default Headers;
